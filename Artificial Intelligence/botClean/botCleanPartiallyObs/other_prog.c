cdcab67be873da424a4a7c529b48bb4ce0ae1755
insidious <andrew.plaza15@gmail.com>
2016-09-15 00:15:20 -0400

    fuckye

--- Artificial Intelligence/botClean/botCleanPartiallyObs/other_prog.c
+++ Artificial Intelligence/botClean/botCleanPartiallyObs/other_prog.c
@@ -1,95 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
-//LL to hold where we've already moved
-struct moves_node {
-  int p[2];
-  struct moves_node *next;
-};
 
-typedef struct moves_node Node;
+#define TRUE 1
+#define FALSE 0
 
-void append_dist(int posxy, int posy);
-void append(Node *newNode);
-
-Node *head = NULL;
 
 void next_move(int posx, int posy, char board[5][5]) 
 {
-    Node *temp = head;
-
-    int ansx=-1000,ansy=-1000;
-    int i,j, contains_d;
-    contains_d = 0; // 0 is false 1 is true
-     
-    char go = 'd';
-
-    for(i=0; i< 5;i++){
-      for(j = 0; j<5;j++){
-        if(board[i][j] == go) contains_d = 1;
-      }
-    } 
-    if(contains_d == 0){
-      while(temp != NULL){
-
-      }
-    
-    }
-
-    //iterate through the board
-    for(i=0;i<5;i++)
-        for(j=0;j<5;j++)         //manhattan distance in one line
-            if(board[i][j]==go && (abs(i-posx)+abs(j-posy))<= (abs(ansx-posx)+abs(ansy-posy)))
-            {
-                ansx = i;
-                ansy = j;
-            }
-    
-    if(ansx>posx) {
-      printf("DOWN");
-      posx++;
-      append_dist(posx,posy);
-    }
-    else if(ansx<posx){
-      printf("UP");
-      posx--;
-      append_dist(posx,posy);
-    }
-    else if(ansy>posy){
-      printf("RIGHT");
-      posy++;
-      append_dist(posx,posy);
-    }
-    else if(ansy<posy) {
-      printf("LEFT");
-      posy--; 
-      append_dist(posx,posy);
-    }
-    else{
-      printf("CLEAN");
-      append_dist(posx, posy);
-    }
-}
-
-void append_dist(int posx, int posy){
-  Node *aNode = NULL;
-  aNode = (Node *) malloc(sizeof(Node));
-  aNode->p[0] = posx;
-  aNode->p[1] = posy;
-  append(aNode);
-}
-
-
-void append(Node *newNode) {
-  Node *temp = head; 
-
-  if (head == NULL){
-    head = newNode;
-    return;
+  char mapped_board[5][5];
+  
+  FILE *fp;
+  fp = fopen("file.txt", "ab+");
+  fread(mapped_board, sizeof(char), 25, fp);
+  if(feof(fp) != 0){
+    clearerr(fp);
+    fseek(fp, SEEK_SET, 0);
   }
-  while (temp->next != NULL) temp = temp->next;
-  temp -> next = newNode;
-  newNode->next = NULL;
+  fclose(fp);
+
+  int ansx=-1000,ansy=-1000;
+  int i,j, contains_d;
+  
+  contains_d = FALSE;
+
+  for(i=0; i< 5;i++){
+    for(j = 0; j<5;j++){
+      if(board[i][j] == 'd'){
+        contains_d = TRUE; 
+        mapped_board[i][j] = 'd';
+      }
+      else if (board[i][j] == '-' && mapped_board[i][j] != '-') mapped_board[i][j] = '-';
+      else if (board[i][j] == 'o' && (mapped_board[i][j] != '-' && mapped_board[i][j] != 'd'))
+      {
+        mapped_board[i][j] = 'o';
+      }
+    }
+  } 
+  
+  fp = fopen("file.txt", "w");
+  fseek(fp, SEEK_SET, 0);
+  fwrite(mapped_board, sizeof(char), 25, fp);
+  if(feof(fp) != 0){
+    //fuck 
+  }
+
+  //iterate through the board
+  for(i=0;i<5;i++){
+      for(j=0;j<5;j++){        //manhattan distance in one line
+        if(contains_d == TRUE){
+           if(board[i][j] == 'd' && (abs(i-posx)+abs(j-posy))<= (abs(ansx-posx)+abs(ansy-posy)))
+          {
+              ansx = i;
+              ansy = j;
+          }
+
+        } else {
+           if(mapped_board[i][j] == 'd' && (abs(i-posx)+abs(j-posy))<= (abs(ansx-posx)+abs(ansy-posy)))
+           { 
+             ansx = i;
+             ansy = j;
+           }
+           else if(mapped_board[i][j] == 'o' && (abs(i-posx)+abs(j-posy))<= (abs(ansx-posx)+abs(ansy-posy)))
+           { 
+              ansx = i;
+              ansy = j;
+           }
+        }
+      }
+  }
+  
+  if(ansx>posx)
+    printf("DOWN");
+  else if(ansx<posx)
+    printf("UP");
+  else if(ansy>posy)
+    printf("RIGHT");
+  else if(ansy<posy)
+    printf("LEFT");
+  else
+    printf("CLEAN");
 }
+
 
 /* Tail starts here */
 int main(void) {
